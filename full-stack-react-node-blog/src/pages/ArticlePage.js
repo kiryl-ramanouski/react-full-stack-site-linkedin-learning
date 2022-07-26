d0ea8f77efb639b83668c6dc39e84a20f839513a
Kirill Romanovskii <whitekvazar@gmail.com>
2022-07-26 17:02:24 +0300

    feat: add header for non existing articles. Use if().

--- full-stack-react-node-blog/src/pages/ArticlePage.js
+++ full-stack-react-node-blog/src/pages/ArticlePage.js
@@ -7,6 +7,11 @@ import articles from '../content/article-content';
 const ArticlePage = () => {
   const { name } = useParams();
   const article = articles.find((item) => item.name === name);
+
+  if (!article) {
+    return <h2 className='header'>Article doesn't exist.</h2>;
+  }
+
   return (
     <>
       <h2 className='header'>{article.title}</h2>
