b5db3f0524328847f32f032cbd2f4ec5871bd07e
Kirill Romanovskii <whitekvazar@gmail.com>
2022-07-27 22:35:46 +0300

    feat: add app.POST for article upVote logic. Use req, res, URL params

--- blog-app-back-end/src/server.js
+++ blog-app-back-end/src/server.js
@@ -3,18 +3,27 @@ import bodyParser from 'body-parser';
 
 const articlesInfo = {
   'learn-react': {
-    upvotes: 0,
+    upVotes: 0,
   },
   'learn-node': {
-    upvotes: 0,
+    upVotes: 0,
   },
   'my-thoughts-on-resumes': {
-    upvotes: 0,
+    upVotes: 0,
   },
 };
 
 const app = express();
 
 app.use(bodyParser.json());
+
+app.post('api/articles/:name/upvote', (req, res) => {
+  const articleName = req.params.name;
+  articlesInfo[articleName].upVotes += 1;
+
+  res
+    .status(200)
+    .send(`${articleName} now has ${articlesInfo[articleName].upVotes} votes`);
+});
 
 app.listen(8000, () => console.log('Server is listening the port 8000'));
