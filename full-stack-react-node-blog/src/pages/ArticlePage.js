86175d9ae9d2b32d72dd5ef456ae9436b79bb5dc
Kirill Romanovskii <whitekvazar@gmail.com>
2022-07-27 13:46:58 +0300

    feat: add other articles to ArticlePage. Use filter(), && +<></>, props

--- full-stack-react-node-blog/src/pages/ArticlePage.js
+++ full-stack-react-node-blog/src/pages/ArticlePage.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
+// Components
+import ArticleList from '../components/ArticleList';
+
 // data
 import articles from '../content/article-content';
 
 const ArticlePage = () => {
   const { name } = useParams();
   const article = articles.find((item) => item.name === name);
+  const otherArticles = articles.filter((item) => item.name !== name);
 
   if (!article) {
     return <h2 className='header'>Article doesn't exist.</h2>;
@@ -24,6 +28,12 @@ const ArticlePage = () => {
           );
         })}
       </>
+      {otherArticles && (
+        <>
+          <h3 className='header'>Other Articles</h3>
+          <ArticleList articles={otherArticles} />
+        </>
+      )}
     </>
   );
 };
